ace91ff3cba62fe84e62c36a71795a917952e9ea
Hamza khan <hamzakhan127109@gmail.com>
2025-07-25 00:12:08 +0500

    solve problem 01

---
+++ Problem_01.py
@@ -0,0 +1,17 @@
+# Print a right-angled triangle pattern using asterisks (*)
+
+num = int(input("Enter the number of rows: " ))
+
+for i in range(1, num+1):
+    print("*" * i)
+    
+    
+    
+    
+# Output
+# Enter the number of rows: 5
+# *
+# **
+# ***
+# ****
+# *****
