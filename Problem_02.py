2df3372264888e7f23196441b1cc734d01d78b01
Hamza khan <hamzakhan127109@gmail.com>
2025-07-25 00:15:42 +0500

    Solved problem 02

---
+++ Problem_02.py
@@ -0,0 +1,17 @@
+# Flipping the Triangle (Reverse Logic)
+
+num = int(input("Enter a number: "))
+
+for i in range(num, 0, -1):
+    print("*" * i)
+    
+    
+    
+# Output
+
+# Enter a number: 5
+# *****
+# ****
+# ***
+# **
+# *
